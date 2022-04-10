87c96be626ce74f13f8c33332f987fc32031965b
Mohamed Yassine Bouhamidi <47503616+mybouhamidi@users.noreply.github.com>
2022-04-10 16:01:51 -0400

    Hide Django secret key

--- ui/Django/glicko_rest/settings.py
+++ ui/Django/glicko_rest/settings.py
@@ -6,9 +6,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-18sm3_c#^$1wjd(l#c7ynvm-l7a9ww7=ag!+zcoiu(9_cfqm6l"
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
